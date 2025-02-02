39fd3a092fadfd7ce0709a048f999e0091f4518c
suaib022 <suaib.109206@gmail.com>
2025-02-02 20:16:10 +0600

    initialized with react, tailwind css & shadcn

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1>Hello People !</h1>
+      <Button>Shadcn</Button>
+    </>
+  );
+}
+
+export default App;
