860242f782ff01ca3f65e0244d88a95e1d919703
suaib022 <suaib.109206@gmail.com>
2025-02-02 20:21:25 +0600

    added antd

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,14 @@
-import { Button } from "./components/ui/button";
+import { Button as ShadcnButton } from "./components/ui/button";
+import { Button as AntdButton } from "antd";
 
 function App() {
   return (
     <>
       <h1>Hello People !</h1>
-      <Button>Shadcn</Button>
+      <ShadcnButton>Shadcn</ShadcnButton>
+      <AntdButton color="default" variant="solid">
+        Antd
+      </AntdButton>
     </>
   );
 }
