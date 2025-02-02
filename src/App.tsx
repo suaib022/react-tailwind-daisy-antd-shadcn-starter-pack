831572188b5c9a375de360992ae1c24fecda4d16
suaib022 <suaib.109206@gmail.com>
2025-02-02 20:32:25 +0600

    added daisyUI

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,13 @@ function App() {
   return (
     <>
       <h1>Hello People !</h1>
-      <ShadcnButton>Shadcn</ShadcnButton>
-      <AntdButton color="default" variant="solid">
-        Antd
-      </AntdButton>
+      <div className="flex justify-evenly items-center my-12 border border-blue-500 py-4">
+        <ShadcnButton variant="destructive">Shadcn</ShadcnButton>
+        <AntdButton color="default" variant="solid">
+          Antd
+        </AntdButton>
+        <button className="btn btn-warning">Warning</button>
+      </div>
     </>
   );
 }
